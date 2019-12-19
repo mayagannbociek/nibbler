6a28c6c13ceca8d2c0d534475067a7b0dc677cf4
mayagannbociek <maya.gannbociek@slu.edu>
2019-12-18 19:49:28 -0600

    added more advanced functions: temperature conversions

---
+++ R/moreadvanced.R
@@ -0,0 +1,28 @@
+#' Fahrenheit to Celsius
+#'
+#' @param temp_F
+#'
+#' @description
+#' Converts any temperature in Fahrenheit to the
+#' Celcius equivalent. Because, well, life skills.
+#'
+#' @export
+tempf2c <- function(temp_F) {
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+}
+
+
+#' Celsius to Fahrenheit
+#'
+#' @param temp_c
+#'
+#' @description
+#' Converts any temperature in Celcius to the
+#' Fahrenheit equivalent. Because, well, life skills.
+#'
+#' @export
+tempc2f <- function(temp_c) {
+  temp_f <- temp_c * (5 / 9) + 32
+  return(temp_f)
+}
